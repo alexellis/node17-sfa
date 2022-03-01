b69f94916233332e8e837d41cffa8bf04d4688dd
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2022-03-01 09:59:49 +0000

    Add FunctionQuery to example
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ template/node17-react/function/react/src/FunctionQuery.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class FunctionQuery extends React.Component {
+  state = {
+    functionRes: 'No result yet'
+  }
+
+  async componentDidMount() {
+    let getURL = window.location.protocol
+    +"//"+ window.location.host+`/`
+    console.log(getURL)
+
+    await axios.post(getURL, 
+      {"input":"test",
+       "window.location.host": window.location.host, 
+       "user-agent": navigator.userAgent
+      })
+    .then(res => {
+      const result = JSON.stringify(res.data);
+      this.setState({functionRes: result});
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.functionRes}
+      </div>
+    )
+  }
+}
