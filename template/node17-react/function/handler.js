6a538ece0865600bd07916468c92b8bb48854745
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2022-03-01 09:31:43 +0000

    Initial
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ template/node17-react/function/handler.js
@@ -0,0 +1,23 @@
+'use strict'
+
+module.exports = async (event, context) => {
+
+  // Redirect any GET requests for the root path to the
+  // React app.
+  if(event.method == "GET" && event.path == "/") {
+    return context
+    .headers({"Location": "/app/"})
+    .status(307)
+    .succeed({})
+  }
+
+  // Any other requests are handled by our function below.
+  const result = {
+    'body': JSON.stringify(event.body),
+    'content-type': event.headers["content-type"]
+  }
+
+  return context
+    .status(200)
+    .succeed(result)
+}
